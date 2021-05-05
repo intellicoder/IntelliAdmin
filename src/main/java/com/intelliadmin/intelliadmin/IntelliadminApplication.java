e9c68ea56b6c6d942d3ddc3384f5d6b4fe131f78
zhuzhi <3300945918@qq.com>
2021-05-05 22:46:29 +0800

    项目初始化

---
+++ src/main/java/com/intelliadmin/intelliadmin/IntelliadminApplication.java
@@ -0,0 +1,13 @@
+package com.intelliadmin.intelliadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelliadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelliadminApplication.class, args);
+	}
+
+}
